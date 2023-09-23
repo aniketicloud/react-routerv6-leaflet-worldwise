732321ec6876f6f426f9a06017902605b5ab4e92
Aniket Pandharabale <aniketicloud@gmail.com>
2023-09-23 23:47:07 +0530

    WIP: dynamic route

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { PageNotFound } from "./pages/PageNotFound";
 import { Login } from "./pages/Login";
 import { CityList } from "./components/CityList";
 import { CountryList } from "./components/CountryList";
+import City from "./components/City";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -47,6 +48,7 @@ export const App = () => {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
+          <Route path="cities/:id" element={<City />} />
           <Route
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
